f07f8e0b994b76856e724b299f06e57014723255
Map987 <99967856+Map987@users.noreply.github.com>
2024-06-18 13:27:35 +0800

    Update inference_realesrgan_video.py

--- inference_realesrgan_video.py
+++ inference_realesrgan_video.py
@@ -190,7 +190,7 @@ def inference_video(args, video_save_path, device=None, total_workers=1, worker_
         netscale = 2
         file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth']
     elif args.model_name == 'realesr-animevideov3':  # x4 VGG-style model (XS size)
-        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=34, upscale=4, act_type='prelu')
         netscale = 4
         file_url = ['https://github.com/Map987/Real-ESRGAN/raw/master/2x_AnimeJaNai_V2_Compact_36k.pth']
     elif args.model_name == 'realesr-general-x4v3':  # x4 VGG-style model (S size)
